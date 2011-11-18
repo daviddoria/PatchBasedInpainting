2d07fd9def549fcf4b057dfcfc1ec81c2de2d969
David Doria <daviddoria@gmail.com>
2011-11-18 16:44:22 -0500

    Added class to allow dragging patches.

---
+++ InteractorStyleImageWithDrag.cpp
@@ -0,0 +1,90 @@
+#include "InteractorStyleImageWithDrag.h"
+
+#include <vtkObjectFactory.h>
+#include <vtkRenderWindowInteractor.h>
+
+vtkStandardNewMacro(InteractorStyleImageWithDrag);
+
+InteractorStyleImageWithDrag::InteractorStyleImageWithDrag()
+{
+  this->ImageStyle = vtkSmartPointer<CustomImageStyle>::New();
+  
+  this->TrackballStyle = vtkSmartPointer<CustomTrackballStyle>::New();
+
+}
+
+void InteractorStyleImageWithDrag::Init()
+{
+
+  this->ImageStyle->SetOtherStyle(this->TrackballStyle);
+  this->TrackballStyle->SetOtherStyle(this->ImageStyle);
+
+  this->Interactor->SetInteractorStyle(this->ImageStyle);
+}
+
+void InteractorStyleImageWithDrag::SetCurrentRenderer(vtkRenderer* renderer)
+{
+  this->ImageStyle->SetCurrentRenderer(renderer);
+  this->TrackballStyle->SetCurrentRenderer(renderer);
+}
+
+void InteractorStyleImageWithDrag::SetImageOrientation(const double* leftToRight, const double* bottomToTop)
+{
+  this->ImageStyle->SetImageOrientation(leftToRight, bottomToTop);
+}
+
+////////////////////////////////
+///// CustomTrackballStyle /////
+////////////////////////////////
+vtkStandardNewMacro(CustomTrackballStyle);
+
+void CustomTrackballStyle::OnLeftButtonDown()
+{
+  //std::cout << "TrackballStyle::OnLeftButtonDown()" << std::endl;
+  // Behave like the middle button instead
+  vtkInteractorStyleTrackballActor::OnMiddleButtonDown();
+}
+
+void CustomTrackballStyle::OnLeftButtonUp()
+{
+  //std::cout << "TrackballStyle::OnLeftButtonUp()" << std::endl;
+  // Behave like the middle button instead
+  vtkInteractorStyleTrackballActor::OnMiddleButtonUp();
+  this->InvokeEvent(this->PatchesMovedEvent, NULL);
+}
+
+void CustomTrackballStyle::OnMiddleButtonDown()
+{
+  this->Interactor->SetInteractorStyle(this->OtherStyle);
+  this->OtherStyle->OnMiddleButtonDown();
+}
+
+void CustomTrackballStyle::OnRightButtonDown()
+{
+  this->Interactor->SetInteractorStyle(this->OtherStyle);
+  this->OtherStyle->OnRightButtonDown();
+}
+
+void CustomTrackballStyle::SetOtherStyle(CustomImageStyle* style)
+{
+  this->OtherStyle = style;
+}
+
+////////////////////////////////
+///// CustomImageStyle /////////
+////////////////////////////////
+
+vtkStandardNewMacro(CustomImageStyle);
+
+void CustomImageStyle::OnLeftButtonDown()
+{
+  //std::cout << "OnLeftButtonDown()" << std::endl;
+  // Behave like the middle button instead
+  this->Interactor->SetInteractorStyle(this->OtherStyle);
+  this->OtherStyle->OnLeftButtonDown();
+}
+
+void CustomImageStyle::SetOtherStyle(CustomTrackballStyle* style)
+{
+  this->OtherStyle = style;
+}
---
+++ InteractorStyleImageWithDrag.h
@@ -0,0 +1,100 @@
+#ifndef InteractorStyleImageWithDrag_H
+#define InteractorStyleImageWithDrag_H
+
+#include <vtkCommand.h>
+#include <vtkInteractorStyleTrackballActor.h>
+#include <vtkSmartPointer.h>
+
+// To use this, you must do, in this order:
+//  this->InteractorStyle->SetCurrentRenderer(this->Renderer);
+//  this->Interactor->SetInteractorStyle(this->InteractorStyle);
+//  this->InteractorStyle->Init();
+    
+class CustomTrackballStyle;
+class CustomImageStyle;
+// Define interaction style
+class InteractorStyleImageWithDrag : public vtkInteractorStyleTrackballActor
+{
+  public:
+    
+    
+    static InteractorStyleImageWithDrag* New();
+    vtkTypeMacro(InteractorStyleImageWithDrag,vtkInteractorStyleTrackballActor);
+
+    InteractorStyleImageWithDrag();
+
+    void Init();
+
+    void SetCurrentRenderer(vtkRenderer* renderer);
+
+    void SetImageOrientation(const double*, const double*);
+    
+        
+    vtkSmartPointer<CustomImageStyle> ImageStyle;
+    vtkSmartPointer<CustomTrackballStyle> TrackballStyle;
+    
+  protected:
+
+    
+
+};
+
+
+////////////////////////////////
+///// CustomImageStyle /////////
+////////////////////////////////
+
+#include <vtkInteractorStyleImage.h>
+
+
+class CustomTrackballStyle;
+
+// Define interaction style
+class CustomImageStyle : public vtkInteractorStyleImage
+{
+  public:
+    static CustomImageStyle* New();
+    vtkTypeMacro(CustomImageStyle,vtkInteractorStyleImage);
+
+    void OnLeftButtonDown();
+
+    void SetOtherStyle(CustomTrackballStyle* style);
+
+  
+private:
+  CustomTrackballStyle* OtherStyle;
+  
+};
+
+class CustomImageStyle;
+
+////////////////////////////////
+///// CustomTrackballStyle /////
+////////////////////////////////
+
+// Define interaction style
+class CustomTrackballStyle : public vtkInteractorStyleTrackballActor
+{
+  public:
+    const static unsigned int PatchesMovedEvent = vtkCommand::UserEvent + 1;
+    
+    static CustomTrackballStyle* New();
+    vtkTypeMacro(CustomTrackballStyle,vtkInteractorStyleTrackballActor);
+
+    void OnLeftButtonDown();
+
+    void OnLeftButtonUp();
+
+    void OnMiddleButtonDown();
+
+    void OnRightButtonDown();
+
+    void SetOtherStyle(CustomImageStyle* style);
+
+private:
+  CustomImageStyle* OtherStyle;
+  
+};
+
+
+#endif
