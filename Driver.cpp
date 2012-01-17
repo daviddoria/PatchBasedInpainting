1ac5831b7dc3236ad10245b4e3e9b1d514685659
David Doria <daviddoria@gmail.com>
2012-01-17 14:07:29 -0500

    Initial setup.

---
+++ Driver.cpp
@@ -0,0 +1,8 @@
+#include "Mask.h"
+#include "Inpainting.h"
+
+int main()
+{
+
+  return 0;
+}
---
+++ Mask.h
@@ -0,0 +1,107 @@
+/*=========================================================================
+ *
+ *  Copyright David Doria 2011 daviddoria@gmail.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+/**
+\class Mask
+\brief This class is a subclass of itkImage that provides the concept of "valid" pixels
+       and hole pixels. Pixels that are any other value should never be used in computations.
+*/
+
+#ifndef Mask_h
+#define Mask_h
+
+// ITK
+#include "itkImage.h"
+
+class Mask : public itk::Image<unsigned char, 2>
+{
+public:
+  /** Standard typedefs. */
+  typedef Mask                       Self;
+  typedef itk::Image< unsigned char, 2> Superclass;
+  typedef itk::SmartPointer< Self >              Pointer;
+  typedef itk::SmartPointer< const Self >        ConstPointer;
+  typedef itk::WeakPointer< const Self >         ConstWeakPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(Mask, Image);
+
+  /** Dimension of the image. */
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      Superclass::ImageDimension);
+
+  /** Types derived from the Superclass */
+  typedef typename Superclass::IndexType IndexType;
+
+  typedef typename Superclass::IOPixelType IOPixelType;
+
+  /** Tyepdef for the functor used to access a neighborhood of pixel
+  * pointers. */
+  typedef itk::NeighborhoodAccessorFunctor< Self >
+  NeighborhoodAccessorFunctorType;
+
+  /** Return the NeighborhoodAccessor functor. This method is called by the
+   * neighborhood iterators. */
+  NeighborhoodAccessorFunctorType GetNeighborhoodAccessor()
+  { return NeighborhoodAccessorFunctorType(); }
+
+  /** Return the NeighborhoodAccessor functor. This method is called by the
+   * neighborhood iterators. */
+  const NeighborhoodAccessorFunctorType GetNeighborhoodAccessor() const
+  { return NeighborhoodAccessorFunctorType(); }
+
+  /** Determine if a pixel is a hole pixel.*/
+  bool IsHole(const itk::Index<2>& index) const;
+
+  /** Determine if an entire region is valid.*/
+  bool IsValid(const itk::ImageRegion<2>& region) const;
+
+  /** Determine if a pixel is valid.*/
+  bool IsValid(const itk::Index<2>& index) const;
+
+  /** Snap the pixel values to either 'hole' or 'valid'.*/
+  void Cleanup();
+
+  /** Specify which value should be considered a hole.*/
+  void SetHoleValue(const unsigned char value);
+
+  /** Specify which value should be considered valid.*/
+  void SetValidValue(const unsigned char value);
+
+  /** Get the value that is considered a hole.*/
+  unsigned char GetHoleValue() const;
+
+  /** Get the value that is considered valid.*/
+  unsigned char GetValidValue() const;
+
+private:
+
+  Mask(const Self &);    //purposely not implemented
+  void operator=(const Self &); //purposely not implemented
+
+  Mask();
+
+  unsigned char HoleValue; // Pixels with this value will be filled.
+  unsigned char ValidValue; // Pixels with this value will not be filled - they are the source region.
+
+};
+
+#endif
