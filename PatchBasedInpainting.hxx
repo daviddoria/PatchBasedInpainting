88cd5a2e48234a91f867da50842bd61938244a5c
mikael <mikael.s.persson@gmail.com>
2012-01-21 10:29:29 -0500

    Added some algorithmic comments on PatchBasedInpainting.hxx

--- PatchBasedInpainting.hxx
+++ PatchBasedInpainting.hxx
@@ -9,15 +9,25 @@ PatchBasedInpainting<TImage>::PatchBasedInpainting(const TImage* const image, co
 template <typename TImage>
 void PatchBasedInpainting<TImage>::Inpaint()
 {
-  while(!IsDone())
+  // NOTE MP : Doesn't this basic scheme seem completely retarded from a performance point of view?
+  //           Maybe these notes will help:
+  while(!IsDone())  // Traverse all the pixels to figure out if a hole-pixel remains, then ...
     {
-    Iterate();
+    Iterate();  // Traverse all the pixels to find the next hole-pixel to fill.. !!
     }
+    
+  // NOTE MP : Normally, an algorithm like this should be implemented by keeping a priority-queue 
+  //           of the hole-pixels to visit, and run until the queue is empty.
 }
 
 template <typename TImage>
 void PatchBasedInpainting<TImage>::Iterate()
 {
+  // NOTE MP : The following two lines should be externalized from this function, because the 
+  //           priorities of the target pixels should drive the traversal. In other words, as 
+  //           with a priority-queue, the top element should be the next to "visit", you shouldn't
+  //           have to do a full search for that top element, the only way to coordinate this is 
+  //           from the traversal algorithm, not here.
   itk::Index<2> targetPixel = DetermineTargetPixel();
   itk::Index<2> sourcePixel = DetermineBestSourcePixel(targetPixel);
 
@@ -27,6 +37,12 @@ void PatchBasedInpainting<TImage>::Iterate()
 template <typename TImage>
 bool PatchBasedInpainting<TImage>::IsDone()
 {
+  // NOTE MP : Shouldn't the algorithm terminate when it is done? 
+  //           In any case, it will certainly be more efficient and practical for the algorithm
+  //           itself to keep track of its incremental progress towards the solution than to have 
+  //           some algorithm like "do a pass, check completion, and repeat".
+  //           IMO, this function is useless.
+  
   // Determine if the hole is entirely filled.
   return false;
 }
@@ -34,6 +50,12 @@ bool PatchBasedInpainting<TImage>::IsDone()
 template <typename TImage>
 itk::Index<2> PatchBasedInpainting<TImage>::DetermineTargetPixel()
 {
+  // NOTE MP : This should be implemented with a priority-queue. 
+  //           There are plenty such implementations available, namely, 
+  //             - the standard std::priority_queue
+  //             - the BGL's n_ary_heap_indirect  (recommended for its flexibility, and static 
+  //               indirection), see the implementation of the breadth_first_search in the BGL.
+  
 //   for all boundary pixels
 //   {
 //     this->PriorityFunction->ComputePriority()
