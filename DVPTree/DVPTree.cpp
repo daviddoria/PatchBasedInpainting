1761b2387bd2183cadb2a19284e096b248ee1789
David Doria <daviddoria@gmail.com>
2012-01-23 14:49:17 -0500

    Working on custom topology. This one was copied from hypercube_topology, but I think it needs to be copied from convex_topology instead.

--- DVPTree/DVPTree.cpp
+++ DVPTree/DVPTree.cpp
@@ -2,24 +2,93 @@
 
 #include "metric_space_search.hpp"
 
+struct CustomType
+{
+  
+};
+
+class CustomTopology
+{
+ public:
+  typedef CustomType point_type;
+  typedef float point_difference_type;
+
+  typedef boost::uniform_01<std::minstd_rand, double> rand_t;
+
+  explicit CustomTopology()
+    : gen_ptr(new std::minstd_rand), rand(new rand_t(*gen_ptr))
+  { }
+
+  point_type random_point() const 
+  {
+    point_type p;
+//     for (std::size_t i = 0; i < Dims; ++i)
+//       p[i] = (*rand)() * scaling;
+    return p;
+  }
+
+  point_type bound(point_type a) const
+  {
+//     BOOST_USING_STD_MIN();
+//     BOOST_USING_STD_MAX();
+    point_type p;
+//     for (std::size_t i = 0; i < Dims; ++i)
+//       p[i] = min BOOST_PREVENT_MACRO_SUBSTITUTION (scaling, max BOOST_PREVENT_MACRO_SUBSTITUTION (-scaling, a[i]));
+    return p;
+  }
+
+  double distance_from_boundary(point_type a) const
+  {
+//     BOOST_USING_STD_MIN();
+//     BOOST_USING_STD_MAX();
+// #ifndef BOOST_NO_STDC_NAMESPACE
+//     using std::abs;
+// #endif
+//     BOOST_STATIC_ASSERT (Dims >= 1);
+//     double dist = abs(scaling - a[0]);
+//     for (std::size_t i = 1; i < Dims; ++i)
+//       dist = min BOOST_PREVENT_MACRO_SUBSTITUTION (dist, abs(scaling - a[i]));
+    float dist = 0;
+    return dist;
+  }
+
+  point_type center() const {
+    point_type result;
+//     for (std::size_t i = 0; i < Dims; ++i)
+//       result[i] = scaling * .5;
+    return result;
+  }
+
+  point_type origin() const {
+    point_type result;
+//     for (std::size_t i = 0; i < Dims; ++i)
+//       result[i] = 0;
+    return result;
+  }
+
+  point_difference_type extent() const {
+    point_difference_type result;
+//     for (std::size_t i = 0; i < Dims; ++i)
+//       result[i] = scaling;
+    return result;
+  }
+
+  double distance(point_type a, point_type b) const
+  {
+    double dist = 0.0f;
+    return dist;
+  }
+  
+ private:
+  std::shared_ptr<std::minstd_rand> gen_ptr;
+  std::shared_ptr<rand_t> rand;
+  double scaling;
+};
+
 namespace boost {
   enum vertex_data_t { vertex_data };
 
   BOOST_INSTALL_PROPERTY(vertex, data);
-};
-
-class custom_topology : public boost::hypercube_topology<6, boost::minstd_rand>
-{
-public:
-  double distance(point a, point b) const
-  {
-    double dist = 0.0f;
-    for(unsigned int i = 0; i < 6; ++i)
-      {
-      dist += fabs(a[i] - b[i]);
-      }
-    return dist;
-  }
 };
 
 int main(int argc, char *argv[])
@@ -28,12 +97,10 @@ int main(int argc, char *argv[])
   //typedef boost::hypercube_topology<dimension, boost::minstd_rand> TopologyType;
   // This distance(a,b) function is the equivialent of matlab's norm(a-b)
 
-  typedef custom_topology TopologyType;
-
   typedef boost::adjacency_list<boost::vecS,
                                 boost::vecS,
                                 boost::undirectedS,
-                                boost::property< boost::vertex_data_t, TopologyType::point_type >
+                                boost::property< boost::vertex_data_t, CustomTopology::point_type >
                                 > Graph;
 
   Graph g;
@@ -42,26 +109,10 @@ int main(int argc, char *argv[])
 
   boost::property_map<Graph, boost::vertex_data_t>::type positionMap = get(boost::vertex_data, g);
 
-  TopologyType myTopology;
-  typedef TopologyType::point_type PointType;
+  CustomTopology myTopology;
+  typedef CustomTopology::point_type PointType;
 
-  {
-  PointType a;
-  for(unsigned int dim = 0; dim < dimension; ++dim)
-    {
-    a[dim] = 1;
-    }
-  PointType b;
-  for(unsigned int dim = 0; dim < dimension; ++dim)
-    {
-    b[dim] = 2;
-    }
-
-  std::cout << "Diff: " << myTopology.distance(a, b) << std::endl;
-  }
-  
-
-  typedef ReaK::pp::dvp_tree<VertexType, TopologyType, boost::property_map<Graph, boost::vertex_data_t>::type > TreeType;
+  typedef ReaK::pp::dvp_tree<VertexType, CustomTopology, boost::property_map<Graph, boost::vertex_data_t>::type > TreeType;
 
   // Add vertices to the graph and corresponding points increasin integer points to the tree.
   // The experiment here is to query the nearest neighbor of a point like (5.2, 5.2, 5.1, 5.3, 5.2, 5.1)
@@ -73,9 +124,9 @@ int main(int argc, char *argv[])
     PointType p;
     for(unsigned int dim = 0; dim < dimension; ++dim)
       {
-      p[dim] = vertexId;
+      //p[dim] = vertexId;
       }
-    boost::put(positionMap, v, p); // segfault on this line
+    boost::put(positionMap, v, p);
   };
 
   // Prefer to initialize the DVP-tree with a filled graph, this way, the entire DVP-tree will be initialized at once (gets best results).
@@ -87,7 +138,7 @@ int main(int argc, char *argv[])
   PointType queryPoint;
   for(unsigned int dim = 0; dim < dimension; ++dim)
     {
-    queryPoint[dim] = 5.2;
+    //queryPoint[dim] = 5.2;
     }
 
   VertexType nearestNeighbor = nearestNeighborFinder(queryPoint, g, myTopology, positionMap);
