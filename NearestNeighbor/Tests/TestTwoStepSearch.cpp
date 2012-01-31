e2e85ef36db76900d18898a01c1143df1f0484f7
David Doria <daviddoria@gmail.com>
2012-01-31 11:04:18 -0500

    Setting up two step search.

---
+++ NearestNeighbor/Tests/TestTwoStepSearch.cpp
@@ -0,0 +1,151 @@
+/*=========================================================================
+ *
+ *  Copyright David Doria 2011 daviddoria@gmail.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+// Pixel descriptors
+#include "PixelDescriptors/ImagePatchPixelDescriptor.h"
+
+// Visitors
+//#include "Visitors/DefaultInpaintingVisitor.hpp"
+#include "Visitors/ImagePatchCreatorVisitor.hpp"
+
+// Nearest neighbors
+#include "NearestNeighbor/TwoStepNearestNeighbor.hpp"
+
+// Topologies
+#include "Topologies/ImagePatchTopology.hpp"
+#include "Topologies/FeatureVectorTopology.hpp"
+
+// Initializers
+//#include "Initializers/InitializeFromMaskImage.hpp"
+#include "Initializers/SimpleInitializer.hpp"
+
+// ITK
+#include "itkImageFileReader.h"
+
+// Boost
+#include <boost/graph/grid_graph.hpp>
+#include <boost/property_map/property_map.hpp>
+
+namespace boost 
+{
+  enum vertex_image_patch_t { vertex_image_patch };
+  BOOST_INSTALL_PROPERTY(vertex, image_patch);
+};
+
+// Run with: Data/trashcan.mha Data/trashcan_mask.mha 15
+int main(int argc, char *argv[])
+{
+  // Verify arguments
+  if(argc != 4)
+    {
+    std::cerr << "Required arguments: image.mha imageMask.mha patchRadius" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // Parse arguments
+  std::string imageFilename = argv[1];
+  std::string maskFilename = argv[2];
+
+  std::stringstream ssPatchRadius;
+  ssPatchRadius << argv[3];
+  unsigned int patch_half_width = 0;
+  ssPatchRadius >> patch_half_width;
+
+  // Output arguments
+  std::cout << "Reading image: " << imageFilename << std::endl;
+  std::cout << "Reading mask: " << maskFilename << std::endl;
+  std::cout << "Patch half width: " << patch_half_width << std::endl;
+
+  typedef FloatVectorImageType ImageType;
+
+  typedef  itk::ImageFileReader<ImageType> ImageReaderType;
+  ImageReaderType::Pointer imageReader = ImageReaderType::New();
+  imageReader->SetFileName(imageFilename.c_str());
+  imageReader->Update();
+
+  ImageType::Pointer image = ImageType::New();
+  ITKHelpers::DeepCopy(imageReader->GetOutput(), image.GetPointer());
+
+  typedef  itk::ImageFileReader<Mask> MaskReaderType;
+
+  MaskReaderType::Pointer maskReader = MaskReaderType::New();
+  maskReader->SetFileName(maskFilename);
+  maskReader->Update();
+  std::cout << "hole pixels: " << maskReader->GetOutput()->CountHolePixels() << std::endl;
+  std::cout << "valid pixels: " << maskReader->GetOutput()->CountValidPixels() << std::endl;
+
+  // Create the graph
+  typedef boost::grid_graph<2> VertexListGraphType;
+  boost::array<std::size_t, 2> graphSideLengths = { { imageReader->GetOutput()->GetLargestPossibleRegion().GetSize()[0],
+                                                      imageReader->GetOutput()->GetLargestPossibleRegion().GetSize()[1] } };
+  VertexListGraphType graph(graphSideLengths);
+  typedef boost::graph_traits<VertexListGraphType>::vertex_descriptor VertexDescriptorType;
+
+  // Create the topologies
+  typedef ImagePatchTopology<ImageType> TopologyType1;
+  TopologyType1 space1;
+  
+  typedef FeatureVectorTopology TopologyType2;
+  TopologyType2 space2;
+
+  // Get the index map
+  typedef boost::property_map<VertexListGraphType, boost::vertex_index_t>::const_type IndexMapType;
+  IndexMapType indexMap(get(boost::vertex_index, graph));
+
+  // Create the descriptor maps.
+  typedef boost::vector_property_map<TopologyType1::point_type, IndexMapType> DescriptorMapType1;
+  DescriptorMapType1 descriptorMap1(num_vertices(graph), indexMap);
+  
+  typedef boost::vector_property_map<TopologyType2::point_type, IndexMapType> DescriptorMapType2;
+  DescriptorMapType2 descriptorMap2(num_vertices(graph), indexMap);
+
+  //DefaultInpaintingVisitor visitor;
+  ImagePatchCreatorVisitor<ImageType, DescriptorMapType> visitor(imageReader->GetOutput(), maskReader->GetOutput(), descriptorMap, patch_half_width);
+
+  // Initialize
+  SimpleInitializer(maskReader->GetOutput(), &visitor, graph);
+
+  // Create the tree
+  std::cout << "Creating tree..." << std::endl;
+  typedef dvp_tree<VertexDescriptorType, TopologyType, DescriptorMapType > TreeType;
+  // TreeType tree(graph, space, descriptorMap); // Create from all positions on the graph_traits - don't want to do this unless all positions on the topology are valid.
+  
+  // std::vector<TopologyType::point_type> validPositions; // Don't store the positions, but rather store the vertex_descriptors
+  std::vector<VertexDescriptorType> validPositions;
+  typename boost::graph_traits<VertexListGraphType>::vertex_iterator vi,vi_end;
+  
+  for (tie(vi,vi_end) = vertices(graph); vi != vi_end; ++vi)
+  {
+    if(get(descriptorMap, *vi).IsFullyValid())
+      {
+      validPositions.push_back(*vi);
+      }
+  }
+
+  std::cout << "There are " << validPositions.size() << " validPositions" << " (out of " << imageReader->GetOutput()->GetLargestPossibleRegion().GetNumberOfPixels() << ")" << std::endl;
+
+  TreeType tree(validPositions.begin(), validPositions.end(), space, descriptorMap);
+  std::cout << "Finished creating tree." << std::endl;
+
+  // Create the nearest neighbor finder
+  typedef TwoStepNearestNeighbor<VertexDescriptorType, TopologyType1, DescriptorMapType1, TopologyType2, DescriptorMapType2> SearchType;
+  SearchType nearestNeighborFinder(space1, descriptorMap1, space2, descriptorMap2, 100);
+  nearestNeighborFinder.DVPTree = &tree;
+
+  return EXIT_SUCCESS;
+}
--- NearestNeighbor/TwoStepNearestNeighbor.hpp
+++ NearestNeighbor/TwoStepNearestNeighbor.hpp
@@ -13,7 +13,7 @@
   * of the graph whose position value is closest to a given position value.
   * \tparam CompareFunction The functor type that can compare two distance measures (strict weak-ordering).
   */
-template <typename VertexDescriptor, typename Topology1, typename PositionMap1, typename Topology2, typename PositionMap2, typename CompareFunction = std::less<double> >
+template <typename VertexDescriptor, typename Topology1, typename PositionMap1, typename Topology2, typename PositionMap2, typename DVPTreeType, typename CompareFunction = std::less<double> >
 struct TwoStepNearestNeighbor
 {
 
@@ -23,23 +23,32 @@ struct TwoStepNearestNeighbor
 
   Topology2 m_topology2;
   PositionMap2 m_positionMap2;
-
+ 
   /** The number of nearest neighbors to use from the first step of the search. */
   unsigned int K;
+  
+  /** The tree to use in the first step of the search. */
+  DVPTreeType* DVPTree;
+
   /**
     * Default constructor.
     * \param compare The comparison functor for ordering the distances (strict weak ordering).
     */
-  TwoStepNearestNeighbor(Topology1 topology1, PositionMap1 positionMap1, Topology2 topology2, PositionMap2 positionMap2, const unsigned int k, CompareFunction compare = CompareFunction()) :
-  m_compare(compare), m_topology1(topology1), m_positionMap1(positionMap1), m_topology2(topology2), m_positionMap2(positionMap2), K(k) { };
+  TwoStepNearestNeighbor(Topology1 topology1, PositionMap1 positionMap1, Topology2 topology2, PositionMap2 positionMap2, const unsigned int k, DVPTreeType* dvpTree, CompareFunction compare = CompareFunction()) :
+  m_compare(compare), m_topology1(topology1), m_positionMap1(positionMap1), m_topology2(topology2), m_positionMap2(positionMap2), K(k), DVPTree(dvpTree)
+  { };
 
   VertexDescriptor find_nearest(VertexDescriptor v)
   {
     // Step 1 - K-NN search on first topology
     std::multimap<float, VertexDescriptor> outputMap;
-    find_nearest(v, outputMap, K);
-    
+    this->DVPtree->find_nearest(v, outputMap, K);
+
     // Step 2 - 1-NN search on result of first search, on second topology
+    typedef linear_neighbor_search<> LinearSearchType;
+    LinearSearchType linearSearch;
+    VertexDescriptor nearestNeighbor = linearSearch(queryPoint, graph, m_topology2, m_positionMap2);
+
   }
 };
 
