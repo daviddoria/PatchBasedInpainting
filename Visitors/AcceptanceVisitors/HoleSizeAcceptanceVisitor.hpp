7fcc6c31d9cd8486812d50bed79a029f8e8f56bc
David Doria <daviddoria@gmail.com>
2012-02-23 17:08:34 -0500

    Change CompositeAcceptanceVisitor to have required and override visitors.
    Wrote HoleSizeAcceptanceVisitor.

---
+++ Visitors/AcceptanceVisitors/HoleSizeAcceptanceVisitor.hpp
@@ -0,0 +1,54 @@
+#ifndef HoleSizeAcceptanceVisitor_HPP
+#define HoleSizeAcceptanceVisitor_HPP
+
+#include <boost/graph/graph_traits.hpp>
+
+// Parent class
+#include "Visitors/AcceptanceVisitors/AcceptanceVisitorParent.h"
+
+// Custom
+#include "Helpers/ITKHelpers.h"
+
+// ITK
+#include "itkImageRegion.h"
+
+/**
+
+ */
+template <typename TGraph>
+struct HoleSizeAcceptanceVisitor : public AcceptanceVisitorParent<TGraph>
+{
+  Mask* MaskImage;
+
+  const unsigned int HalfWidth;
+
+  float HolePixelRatio;
+  
+  typedef typename boost::graph_traits<TGraph>::vertex_descriptor VertexDescriptorType;
+
+  HoleSizeAcceptanceVisitor(Mask* const mask, const unsigned int halfWidth, const float holePixelRatio, const std::string& visitorName = "HoleSizeAcceptanceVisitor") :
+  AcceptanceVisitorParent<TGraph>(visitorName), MaskImage(mask), HalfWidth(halfWidth), HolePixelRatio(holePixelRatio)
+  {
+
+  }
+
+  bool AcceptMatch(VertexDescriptorType target, VertexDescriptorType source, float& computedEnergy) const
+  {
+    itk::Index<2> targetPixel = ITKHelpers::CreateIndex(target);
+    itk::ImageRegion<2> targetRegion = ITKHelpers::GetRegionInRadiusAroundPixel(targetPixel, HalfWidth);
+
+    unsigned int numberOfHolePixels = MaskImage->CountHolePixels();
+
+    if(static_cast<float>(numberOfHolePixels)/static_cast<float>(targetRegion.GetNumberOfPixels()) < HolePixelRatio)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  };
+
+};
+
+#endif
