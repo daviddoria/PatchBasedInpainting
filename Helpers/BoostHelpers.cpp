05db4791e734aabd8e5fbc9f5f8485009dd4f3f0
David Doria <daviddoria@gmail.com>
2012-03-29 14:57:58 -0400

    Replacing helpers with submodules.

--- Helpers/BoostHelpers.cpp
+++
@@ -1,8 +0,0 @@
-#include "BoostHelpers.h"
-
-std::ostream& operator<<(std::ostream& output, const boost::array<size_t, 2>& vertexDescriptor)
-{
-  output << "(" << vertexDescriptor[0] << ", " << vertexDescriptor[1] << ")" << std::endl;
-
-  return output;
-}
